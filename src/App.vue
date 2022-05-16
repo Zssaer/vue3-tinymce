636f9124925b966816122ff2f14d3076d56782c8
Zssaer <ntc001@163.com>
2022-05-16 17:08:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TEditor from '@/TEditor.vue'
+import { ref } from 'vue';
+const content =ref('');
+</script>
+
+<template>
+  <TEditor ref="editor" v-model="content"/>
+</template>
+
+<style>
+
+</style>
