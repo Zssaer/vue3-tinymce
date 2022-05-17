2cc1ba7617ad6cbddbf96d08a21f6cdf6f3dc489
Zssaer <ntc001@163.com>
2022-05-17 11:33:31 +0800

    npm config

---
+++ src/TinymceEditor.ts
@@ -0,0 +1,2 @@
+import TinyMceEditor from './App.vue'
+export default TinyMceEditor;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
+import { resolve } from "path";
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, "src/TinymceEditor.ts"),
+      // main: resolve(__dirname, "index.html"),
+      output: {
+        entryFileNames: "TinymceEditor.js",
+        manualChunks: {
+          tinymce: ['tinymce']
+        },
+        chunkFileNames: "assets/tinymce.js",
+      },
+    },
+    minify: "terser", 
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  plugins: [vue()],
+  plugins: [vue(),visualizer()],
+  
 });
